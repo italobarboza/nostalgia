20277de7b04950229568dc73dcaab2790a3c68e0
ZYX <eke00372@gmail.com>
2024-08-29 06:24:21 +0800

    Add zhTW/zhCN

---
+++ Locales/zhTW.lua
@@ -0,0 +1,30 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("Nostalgia", "zhTW")
+
+L["Manual command"] = "手動指令"
+L["Guild Achievement earned"] = "獲得公會成就"
+L["Personal Achievement earned"] = "獲得個人成就"
+L["Level up"] = "升級"
+L["Trade completed"] = "交易完成"
+L["Boss killed: "] = "擊殺首領："
+L["PvP event victory"] = "PvP 獲勝"
+L["General Settings"] = "一般設定"
+L["About"] = "關於"
+L["Take Screenshot on Personal Achievements"] = "個人成就截圖"
+L["Enable or disable taking screenshots when you earn a personal achievement."] =
+    "獲得個人成就時自動截圖。"
+L["Take Screenshot on Guild Achievements"] = "公會成就截圖"
+L["Enable or disable taking screenshots when your guild earns an achievement."] =
+    "公會獲得成就時自動截圖。"
+L["Take Screenshot on Boss Kill"] = "擊殺首領時截圖"
+L["Enable or disable taking screenshots when you kill a boss."] =
+    "擊殺首領時自動截圖。"
+L["Take Screenshot on PvP Event"] = "PvP活動截圖"
+L["Enable or disable taking screenshots during PvP events."] = "在PvP活動期間自動截圖。"
+L["Take Screenshot on Level Up"] = "升級時截圖"
+L["Enable or disable taking screenshots when you level up."] = "升級時自動截圖。"
+
+
+
+
+
+
